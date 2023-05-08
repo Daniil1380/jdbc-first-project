a0b3e743b87482626a6f1d1e0c3b5c75b7aff179
d.tkachenko <d.tkachenko@argustelecom.ru>
2023-05-08 20:52:44 +0300

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,14 +3,14 @@ package org.example;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
         Connection connection =
                 DriverManager.getConnection("jdbc:postgresql://localhost:5433/postgres", "postgres", "qwerty");
-
-
-
-        System.out.println("Hello world!");
+        Statement statement = connection.createStatement();
+        statement.execute("INSERT INTO cars VALUES (3, 'AUDI')");
+        System.out.println("Connected!");
     }
 }
